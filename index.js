86482236b8f80d2b32f86a71510c78a5d298286e
Santiago Trini <santiagotrini@gmail.com>
2019-11-14 17:43:32 -0300

    cosas

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const express = require('express');
 const mongoose =  require('mongoose');
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+// la ruta raiz usa html pelado con sendFile y la carpeta public
 app.get ('/',(req,res)=>{
-  res.send('jeje');
+  res.sendFile('index.html');
 });
+
+// la ruta garavagno usa plantillas con Pug
+app.get('/garavagno', (req, res) => {
+  res.render('garavagno', { usuario: 'Santiago', numero: 99 });
+});
+
 app.listen(port);
